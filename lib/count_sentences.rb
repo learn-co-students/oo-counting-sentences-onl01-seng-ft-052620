fe8d1088aa31818bd8abe9586640f5b36c1874cf
MiikeT <mikemtaki@gmail.com>
2020-05-29 09:22:32 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+   self.end_with?("?") 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sen_split = self.split(".")
+    ex_split = sen_split.join("!").split("!")
+    q_split = ex_split.join("?").split("?")
+    q_split.reject(&:empty?).length
   end
 end
