61701108cfe7e2c32ae4de6f56e8d809872b286e
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-06-03 17:47:30 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with? "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with? "?"
+      true
+    else
+      false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with? "!"
+      true
+    else
+      false 
+    end
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
+  
   end
+
+
+
+
+  # def count_sentences
+  #   array = []
+  #   array = self.split(/\W+/)
+  #   array.map.remove(" ").count
+  #   binding.pry 
+  # end
 end
