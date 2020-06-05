9febb699d61f41daf7ff53c98394d942a2dfb501
francepants <franceline90@gmail.com>
2020-06-05 09:19:51 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") #true if it ends in a "." and false if not
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).count
   end
 end
