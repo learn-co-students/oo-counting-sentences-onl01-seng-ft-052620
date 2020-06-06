44b2ba385fa7538bed583597fd2110a75d38bca2
ejguzman3988 <eriberto176@gmail.com>
2020-06-06 07:30:01 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,67 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+
+    count = 0
+
+    store_string = []
+
+    if self.empty?
+      return 0
+    end
+
+    if self.sentence?
+       store_string = self.split(".")
+       store_string.delete("")
+      count = count + store_string.length
+      
+    else
+      store_string = self.split(".")
+      store_string.delete("")
+      count = count + store_string.length - 1
+      
+    end
+    
+    if self.exclamation?
+       store_string = self.split("!")
+       store_string.delete("")
+      count = count + store_string.length
+      
+    else
+      store_string = self.split("!")
+      store_string.delete("")
+      count = count + store_string.length - 1
+      
+    end
+    
+    if self.question?
+       store_string = self.split("?")
+       store_string.delete("")
+      count = count + store_string.length
+      
+    else
+      store_string = self.split("?")
+      store_string.delete("")
+      count = count + store_string.length - 1
+      
+    end
+    
+    count
+
+
 
   end
 end
